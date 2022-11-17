09e8161760db8f8f64420625d4494f22416a8588
Steven Hunt <sph@shunty.co.uk>
2022-11-17 18:03:00 +0000

    Minimize d1 Rust

--- rs/src/days/day01.rs
+++ rs/src/days/day01.rs
@@ -1,10 +1,11 @@
-use crate::common::*;
+//use crate::common::*;
 
 pub fn run() {
-    let day = 1;
-    let v = get_input_ints(&day).unwrap();
+    // let day = 1;
+    // let v = get_input_ints(&day).unwrap();
 
-    let (mut part1, mut part2) = (0, 0);
+    // let (mut part1, mut part2) = (0, 0);
 
-    print_day_results(&day, &part1, &part2);
+    // print_day_results(&day, &part1, &part2);
+    println!("Hello Advent of code - waiting for day 1 data...")
 }
