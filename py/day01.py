b4ea3f7013474d75fa5bf65a6283f8a5c3d8781d
Steven Hunt <sph@shunty.co.uk>
2022-12-02 12:45:43 +0000

    Minor improvement to d2 py

--- py/day01.py
+++ py/day01.py
@@ -1,5 +1,7 @@
 import os
 
+# https://adventofcode.com/2022/day/1 - Day 1: Calorie Counting
+
 #input = [line.strip() for line in '1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000'.split('\n')]
 with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day01-input", "r") as f:
     input = [line.strip() for line in f.readlines()]
--- py/day02.py
+++ py/day02.py
@@ -1,45 +1,32 @@
 import os
 
-def points_for(x):
-    return 1 if x == "A" else 2 if x == "B" else 3
+# https://adventofcode.com/2022/day/2 - Day 2: Rock Paper Scissors
+# Why does modulo stuff mess with my brain so much
 
-def get_score(a,b):
-    p = points_for(b)
-    if a == b:
-        return 3 + p
-    elif a == "A":
-        if b == "B":
-            return 6 + p
-        else:
-            return p
-    elif a == "B":
-        if b == "A":
-            return p
-        else:
-            return 6 + p
-    else:
-        if b == "A":
-            return 6 + p
-        else:
-            return p
+def points_for(x):
+    return 1 if (x == "A" or x == "X") else 2 if (x == "B" or x == "Y") else 3
+
+def get_score(a, b):
+    pt_a = points_for(a)
+    pt_b = points_for(b)
+    win = pt_b % 3 == (pt_a + 1) % 3
+    return pt_b + (3 if pt_a == pt_b else 6 if win else 0)
 
 with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day02-input", "r") as f:
     input = [line.strip() for line in f.readlines()]
 
-score1 = 0
-score2 = 0
-map = { "X": "A", "Y": "B", "Z": "C" }
-for i in input:
-    sp = i.split(' ')
-    score1 += get_score(sp[0], map[sp[1]])
-    if sp[1] == "X": # Lose
-        play = "C" if sp[0] == "A" else "A" if sp[0] == "B" else "B"
-        score2 += get_score(sp[0], play)
-    elif sp[1] == "Y": # Draw
-        score2 += get_score(sp[0], sp[0])
-    else: # Win
-        play = "B" if sp[0] == "A" else "C" if sp[0] == "B" else "A"
-        score2 += get_score(sp[0], play)
+part1 = 0 # sum(get_score(p1, p2) for (p1, p2) in [(play[0], play[1]) for play in [i.strip().split(' ') for i in input]])
+part2 = 0
 
-print("Part 1:", score1)
-print("Part 2:", score2)
+for (p1, p2) in [(play[0], play[1]) for play in [i.split(' ') for i in input]]:
+    part1 += get_score(p1, p2)
+    pp1 = points_for(p1)
+    if p2 == "X": # Lose
+        part2 += 3 if pp1 == 1 else (pp1 - 1) # or ((pp1 - 1 - 1 + 3) % 3) + 1 == ((pp1 + 1) % 3) + 1
+    elif p2 == "Y": # Draw
+        part2 += 3 + pp1
+    else: # Win
+        part2 += 6 + (1 if pp1 == 3 else (pp1 + 1)) # or ((pp1 - 1 + 1 + 3) % 3) + 1 == (pp1 % 3) + 1
+
+print("Part 1:", part1)
+print("Part 2:", part2)
