7b5897ab117c129d2b488dd7f00c3db73793cd36
Steven Hunt <sph@shunty.co.uk>
2022-12-09 14:19:39 +0000

    Day 9, py

--- py/day09.py
+++ py/day09.py
@@ -1,71 +1,48 @@
 import os
 from collections import Counter
 
-# https://adventofcode.com/2022/day/9
+# https://adventofcode.com/2022/day/9 - Day 9: Rope Bridge
 
 def neighbours(p: tuple[int,int]) -> list[tuple[int,int]]:
     p0,p1 = p
     return [
-        (p0, p1),
-        (p0, p1-1),
-        (p0+1, p1-1),
-        (p0+1, p1),
-        (p0+1, p1+1),
-        (p0, p1+1),
-        (p0-1, p1+1),
-        (p0-1, p1),
-        (p0-1, p1-1)]
-
-def is_adjacent(hh, tt) -> bool:
-    return tt in neighbours(hh)
+        (p0, p1), (p0, p1-1), (p0+1, p1-1),
+        (p0+1, p1), (p0+1, p1+1), (p0, p1+1),
+        (p0-1, p1+1), (p0-1, p1), (p0-1, p1-1)
+        ]
 
 def move_tail(hh: tuple[int, int], tt: tuple[int, int]) -> tuple[int, int]:
-    if is_adjacent(hh, tt):
+    if (tt in neighbours(hh)):
         return (0,0)
 
     if hh[0] == tt[0]:
-        return (tt[0], tt[1]+1) if hh[1] > tt[1] else (tt[0], tt[1]-1)
+        return (0, 1) if hh[1] > tt[1] else (0, -1)
     elif hh[1] == tt[1]:
-        return (tt[0]+1, tt[1]) if hh[0] > tt[0] else (tt[0]-1, tt[1])
+        return (1, 0) if hh[0] > tt[0] else (-1, 0)
     else: # diagonal
-        if hh[0] > tt[0] and hh[1] > tt[1]:
-            return (tt[0]+1, tt[1]+1)
-        elif hh[0] > tt[0] and hh[1] < tt[1]:
-            return (tt[0]+1, tt[1]-1)
-        elif hh[0] < tt[0] and hh[1] < tt[1]:
-            return (tt[0]-1, tt[1]-1)
-        else:
-            return (tt[0]-1, tt[1]+1)
+        tt0 = 1 if hh[0] > tt[0] else -1
+        tt1 = 1 if hh[1] > tt[1] else -1
+        return (tt0, tt1)
 
 def do_moves(moves: list[tuple[str, int]], num_tails: int) -> int:
-    dirs = { 'L': (-1, 0), 'R': (1,0), 'U': (0, -1), 'D': (0, 1)}
-    v = Counter()
-    head: tuple[int,int] = (0,0)
-    tails: list[tuple[int,int]] = [(0,0)] * num_tails
-    v[(0,0)] = 1
-    for move in moves:
-        dir = dirs[move[0]]
-        count = move[1]
-        for i in range(count):
-            head = (head[0] + dir[0], head[1] + dir[1])
-            for ti in range(len(tails)):
-                t0 = head if ti == 0 else tails[ti-1]
-                t1 = tails[ti]
+    dir_deltas = { 'L': (-1, 0), 'R': (1,0), 'U': (0, -1), 'D': (0, 1)}
+    visited = Counter()
+    rope: list[tuple[int,int]] = [(0,0)] * (num_tails+1)
+    visited[(0,0)] = 1
+    for dir, dist in moves:
+        dx,dy = dir_deltas[dir]
+        for _ in range(dist):
+            rope[0] = (rope[0][0] + dx, rope[0][1] + dy)
+            for ti in range(1, len(rope)):
+                t0 = rope[ti-1]
+                t1 = rope[ti]
                 t_move = move_tail(t0, t1)
-                if (t_move[0] != 0 or t_move[1] != 0):
-                    tails[ti] = t_move
-                    if ti == num_tails - 1:
-                        v[t_move] += 1
-    return len(v)
+                rope[ti] = (t1[0]+t_move[0], t1[1]+t_move[1])
+                if ti == num_tails:  # strictly speaking '...and t_move != (0,0)'
+                    visited[rope[ti]] += 1
+    return len(visited)
 
-test_input = """R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2"""
+#test_input = "R 4\nU 4\n\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
 #lines = test_input.split('\n')
 with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day09-input", "r") as f:
     lines = [line.strip() for line in f.readlines()]
