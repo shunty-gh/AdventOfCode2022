2fa635f2c77df283f30fa5d9e0e6a5eaaaf0d97e
Steven Hunt <sph@shunty.co.uk>
2022-12-12 13:50:53 +0000

    Day 12, py

--- py/day12.py
+++ py/day12.py
@@ -1,6 +1,5 @@
-from __future__ import annotations
-import heapq, os, sys
-from collections import defaultdict
+import os, sys
+from collections import defaultdict, deque
 
 # https://adventofcode.com/2022/day/12 - Day 12: Hill Climbing Algorithm
 
@@ -12,62 +11,47 @@ def find_shortest(starts: list[tuple[int,int]], map: list[str]):
     maxX, maxY = len(map[0]), len(map)
     shortest_of_all = sys.maxsize
     for start in starts:
-        to_visit = [(start,0)]
+        to_visit: deque[tuple[tuple[int,int],int]] = deque([(start,0)])
         costs = defaultdict(lambda: sys.maxsize)
         costs[start] = 0
         shortest = sys.maxsize
         while len(to_visit) > 0:
-            (x,y),c = heapq.heappop(to_visit)
+            (x,y),c = to_visit.popleft()
             if c >= shortest or c > costs[(x,y)]:
                 continue
 
             curr = ord('a') if map[y][x] == 'S' else ord(map[y][x])
-            neighbours = get_neighbours(x, y, maxX, maxY)
-            for [nx,ny] in neighbours:
+            for [nx,ny] in get_neighbours(x, y, maxX, maxY):
                 nv = ord('z') if map[ny][nx] == 'E' else ord(map[ny][nx])
-                can_visit = nv <= curr + 1
-                if not can_visit:
+                if nv > curr + 1:
                     continue
-                cost = c + 1
 
+                cost = c + 1
                 if cost < costs[(nx,ny)]:
                     costs[(nx,ny)] = cost
                     if map[ny][nx] == 'E':
                         shortest = cost
                     else:
-                        heapq.heappush(to_visit, ((nx,ny), cost))
+                        to_visit.append(((nx,ny), cost))
 
         if shortest < shortest_of_all:
             shortest_of_all = shortest
 
     return shortest_of_all
 
-# test_input = """Sabqponm
-# abcryxxl
-# accszExk
-# acctuvwj
-# abdefghi"""
-# lines = [line.strip() for line in test_input.splitlines()]
+#lines = """Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi""".splitlines()
 with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day12-input", "r") as f:
     lines = [line.strip() for line in f.readlines()]
 
-# find the start
-start = (0,0)
+# P1: find the start position ( == 'S')
+sy = next(y for y,line in enumerate(lines) if 'S' in line)
+start = (lines[sy].index('S'),sy)
+p1 = find_shortest([start], lines)
+
+# P2: find all potential starts ( == 'a' or == 'S)
+possible_starts = [start]
 for y,line in enumerate(lines):
-    if 'S' in line:
-        x = line.index('S')
-        start = (x,y)
-        break
-
-p1 = find_shortest([(start)], lines)
-
-# find all potential starts ( == 'a' or == 'S)
-possible_starts = []
-for y,line in enumerate(lines):
-    for x,ch in enumerate(line):
-        if ch == 'a' or ch == 'S':
-            possible_starts.append((x,y))
-
+    possible_starts.extend([(x,y) for x,ch in enumerate(line) if ch == 'a'])
 p2 = find_shortest(possible_starts, lines)
 
 print("Part 1: ", p1)
