e5ad2095653ffa1485d7e5318be448b2266b2ef5
Steven Hunt <sph@shunty.co.uk>
2022-12-23 10:20:51 +0000

    Day 23, py

--- py/day23.py
+++ py/day23.py
@@ -1,7 +1,7 @@
 import os, re
 from collections import defaultdict
 
-# https://adventofcode.com/2022/day/23
+# https://adventofcode.com/2022/day/23 - Day 23: Unstable Diffusion
 
 DELTAS = [
     [(0,-1),(1,-1),(-1,-1)],  # N NE NW
@@ -26,18 +26,27 @@ def print_elves(elves):
         print(ln)
     print()
 
+def free_space(elves):
+    x_min = min([x for (x,_) in elves])
+    y_min = min([y for (_,y) in elves])
+    x_max = max([x for (x,_) in elves])
+    y_max = max([y for (_,y) in elves])
+    result = 0
+    for y in range(y_min, y_max+1):
+        for x in range(x_min, x_max+1):
+            if not (x,y) in elves:
+                result += 1
+    return result
 
-#with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day23-test", "r") as f:
-with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day23-input", "r") as f:
-    lines = [line.strip() for line in f.readlines()]
-elves = set()
-for y,line in enumerate(lines):
-    for x,ch in enumerate(line):
-        if ch == '#':
-            elves.add((x,y))
+def load_elves(data):
+    result = set()
+    for y,line in enumerate(data):
+        for x,ch in enumerate(line):
+            if ch == '#':
+                result.add((x,y))
+    return result
 
-dstart = 0
-for _ in range(10):
+def do_round(elves, dstart):
     next = defaultdict(lambda: [])
     for (ex,ey) in elves:
         all_empty = True
@@ -72,19 +81,38 @@ for _ in range(10):
                 elves.add((nx,ny))
         else:
             elves.add((next_x, next_y))
+    return elves
 
+def compare_elves(e1, e2):
+    for e in e1:
+        if not e in e2:
+            return False
+    return True
+
+#with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day23-test", "r") as f:
+with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day23-input", "r") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+elves = load_elves(lines)
+dstart = 0
+for _ in range(10):
+    elves = do_round(elves, dstart)
     dstart = (dstart + 1) % 4
 
-x_min = min([x for (x,_) in elves])
-y_min = min([y for (_,y) in elves])
-x_max = max([x for (x,_) in elves])
-y_max = max([y for (_,y) in elves])
-p1 = 0
-for y in range(y_min, y_max+1):
-    for x in range(x_min, x_max+1):
-        if not (x,y) in elves:
-            p1 += 1
-
 #print_elves(elves)
-print("Part 1: ", p1)
-print("Part 2: ", )
+print("Part 1: ", free_space(elves))
+
+## Part 2
+# Not particularly efficient but it works quick enough
+elves = load_elves(lines)
+dstart = 0
+round = 1
+while True:
+    elves1 = do_round(elves, dstart)
+    if compare_elves(elves, elves1):
+        break
+    dstart = (dstart + 1) % 4
+    round += 1
+    elves = elves1
+
+print("Part 2: ", round)
