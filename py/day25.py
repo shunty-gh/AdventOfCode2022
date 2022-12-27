06d1e9f3307ca2cc32eefddf71ac41c65d568c9f
Steven Hunt <sph@shunty.co.uk>
2022-12-27 10:08:52 +0000

    Day 25 tidy up

--- py/day25.py
+++ py/day25.py
@@ -1,6 +1,6 @@
 import os
 
-# https://adventofcode.com/2022/day/25
+# https://adventofcode.com/2022/day/25 - Day 25: Full of Hot Air
 
 def fromSNAFU(snum):
     result = 0
@@ -31,14 +31,13 @@ def to_base5(n):
     result.reverse()
     return result
 
-def to_SNAFU(src):
+def to_SNAFU(num):
+    src = to_base5(num)
     result = []
-    sl = len(src)
-    s = src.copy()
-    s.reverse()
-    while len(s)>0:
-        v = s[0]
-        s = s[1:]
+    src.reverse()
+    while len(src)>0:
+        v = src[0]
+        src = src[1:]
         if v <= 2:
             result.append(v)
         elif v == 3 or v == 4:
@@ -48,23 +47,22 @@ def to_SNAFU(src):
                 result.append('-')
             i = 0
             while True:
-                if len(s) > i:
-                    s[i] += 1
+                if len(src) > i:
+                    src[i] += 1
                 else:
-                    s.append(1)
-                if s[i] == 5:
-                    s[i] = 0
+                    src.append(1)
+                if src[i] == 5:
+                    src[i] = 0
                     i += 1
                 else:
                     break
     result.reverse()
-    return result
+    return ''.join([str(n) for n in result])
 
 # with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day25-test", "r") as f:
 with open(os.path.dirname(os.path.realpath(__file__)) + "/../input/day25-input", "r") as f:
     lines = [line.strip() for line in f.readlines()]
-sum = sum([fromSNAFU(s) for s in lines])
-b5 = to_base5(sum)
-p1 = to_SNAFU(b5)
-print("Part 1: ", sum, b5,p1)
-print("Part 1: ", ''.join([str(p) for p in p1]))
+p1 = sum([fromSNAFU(s) for s in lines])
+p2 = to_SNAFU(p1)
+print("Part 1: ", p1)
+print("Part 2: ", p2)
